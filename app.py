9a735390aabd81272ab747f93d8cb3b5d4b33ad3
zhenpin yu <zhenpinyu@zhenpindeMacBook-Pro.local>
2017-11-18 05:34:07 +0800

    Add APIs

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from tinydb import TinyDB, Query, where
+
+toilets = TinyDB('toilet.json')
+usage = TinyDB('usage.json')
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,6 +21,34 @@ class TodoSimple(Resource):
 
 
 api.add_resource(TodoSimple, '/<string:todo_id>')
+
+# GET /toilets
+# GET /toilets?available=true
+# GET /toilets/{id}
+# PUT /toilets/{id}
+# DEL /toilets/{id}
+
+class Toilets(Resource):
+    def get(self):
+        return toilets.all()
+
+class Toilet(Resource):
+    def get(self, id):
+        return toilets.search(where('id') == id)
+
+    def put(self, id):
+        available = request.form["available"]
+        print "available: %s" % (available)
+        if available == "true":
+            toilets.update({'available': True}, where('id') == id)
+        elif available == "false":
+            toilets.update({'available': False}, where('id') == id)
+        else:
+            print "Parameter error: %s" %(available)
+        return toilets.search(where('id') == id)
+
+api.add_resource(Toilets, "/toilets")
+api.add_resource(Toilet, "/toilets/<int:id>")
 
 if __name__ == '__main__':
     app.run(debug=True)
